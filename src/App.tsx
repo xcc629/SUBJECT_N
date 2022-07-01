a43881879f416a8da428b15fb44bff7a8f3b10a0
geunhong Lim <seelime11@gmail.com>
2022-07-01 11:35:36 +0900

    feat: 로그인 UI #5

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './pages/login';
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </BrowserRouter>
   );
